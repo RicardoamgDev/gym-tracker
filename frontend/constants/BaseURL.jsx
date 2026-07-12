203f092fb1766a67f25f5f3db17d9e272a0e8dc8
Ricardo Munoz <138828776+RicardoamgDev@users.noreply.github.com>
2026-07-12 12:48:44 -0400

    s

--- frontend/constants/BaseURL.jsx
+++ frontend/constants/BaseURL.jsx
@@ -2,7 +2,7 @@
 //export const BASE_URL = "http://127.0.0.1:8000";
 
 //Produccion Hetzner
-export const BASE_URL = "https://gymtracker.softwarech.cl";
+export const BASE_URL = "https://apigymtracker.softwarech.cl";
 
 // Helper para rutas de archivos públicos
 export const storageUrl = (path) => `${BASE_URL}/storage/${path}`;
